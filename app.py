1e685146e4ac6d80d82ef75e5947c31480577034
Owain Lewis <owain@owainlewis.com>
2015-12-10 21:12:49 +0000

    updates layouts

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+from parser import SwaggerParser
+
 app = Flask(__name__)
 
+parser = SwaggerParser("spec.yaml")
+
 @app.route("/")
-def template_test():
-     return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+def index():
+     return render_template('index.html', operations = parser.paths())
 
 if __name__ == '__main__':
     app.run(debug=True)
