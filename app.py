aa0d77c2d939610f9dec98f3c02b9bf23a14665e
Owain Lewis <owain@owainlewis.com>
2015-12-15 21:12:20 +0000

    updates layouts and styles

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 import os
 from flask  import Flask, render_template
 from parser import SwaggerParser
-
+from os import listdir
+from os.path import isfile, join
+     
 app = Flask(__name__)
 
-parser = SwaggerParser("static/specs/examples.yaml")
+SPEC_PATH = "static/specs"
+
+def spec_exists(spec):
+     return isfile(join(SPEC_PATH, spec))
+
+def load_specs():
+     return [f.replace('.yaml','') for f in listdir(SPEC_PATH) if spec_exists(f)]
+
+@app.route("/docs/<name>")
+def spec(name):
+     parser = SwaggerParser("static/specs/%s.yaml" % name)
+     return render_template('spec.html', spec = parser.data, operations = parser.operations())
 
 @app.route("/")
 def index():
-     return render_template('index.html', spec = parser.data, operations = parser.operations())
+     return render_template('intro.html', specs=load_specs())
 
 PORT = os.getenv('VCAP_APP_PORT', '5000')
-
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=int(PORT))
+	app.run(host='0.0.0.0', port=int(PORT), debug=True)
