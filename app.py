ee432a5e4cec08d21f9da023d5ab330ad3d45f85
Owain Lewis <owain@owainlewis.com>
2015-12-15 10:33:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask  import Flask, render_template
 from parser import SwaggerParser
 
@@ -9,5 +10,7 @@ parser = SwaggerParser("static/specs/examples.yaml")
 def index():
      return render_template('index.html', spec = parser.data, operations = parser.operations())
 
-if __name__ == '__main__':
-    app.run(debug=True)
+PORT = os.getenv('VCAP_APP_PORT', '5000')
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=int(PORT))
